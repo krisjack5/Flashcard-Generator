e11ac860902aabf7629405fa0d5a955f9af200fa
krisjack5 <krisjack29@gmail.comkrisjack5>
2017-04-08 16:25:20 -0400

    final

---
+++ ClozeCard.js
@@ -0,0 +1,25 @@
+var fs = require("fs");
+
+module.exports = ClozeFlashcard;
+
+// constructor for ClozeFlashcard
+function ClozeFlashcard(text, cloze) {
+    this.text = text;
+    this.cloze = cloze;
+    this.clozeDeleted = this.text.replace(this.cloze, '_____');
+    this.create = function() {
+        var data = {
+            text: this.text,
+            cloze: this.cloze,
+            clozeDeleted: this.clozeDeleted,
+            type: "cloze"
+        };
+        
+        fs.appendFile("log.txt", JSON.stringify(data) + ';', "utf8", function(error) {
+       
+            if (error) {
+                console.log(error);
+            }
+        });
+    };
+}
